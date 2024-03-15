0446a1f474e7faf64135e3c689cdbdf80af082c5
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-03-15 20:35:14 +0530

    new project 02 added

---
+++ 02basicreact/dummy project2/src/deepesh.jsx
@@ -0,0 +1,15 @@
+ 
+// make new component
+ function Deepesh() {
+  
+    return (
+      <>
+       <h1>hello deepesh bhai aap ne react me new compenet bana liya.  </h1>
+        
+        <p>Thanks Hitesh sir for such content</p>
+       </>
+    )
+  }
+  
+  export default Deepesh;
+  
