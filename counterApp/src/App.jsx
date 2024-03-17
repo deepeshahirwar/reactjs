8e618839d1fb8bdbffd442f53aeb90112b391100
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-03-18 00:36:28 +0530

    counter app create | lecture 4

---
+++ counterApp/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { 
+ 
+  let  [counter , setCounter] = useState(5);
+
+  // let counter = 5; 
+ 
+  const addValue = ()=>{ 
+      if(counter > 20){ // counter value not exceed 20
+        counter = 20;
+      }else{
+        setCounter(counter++);
+      }
+    
+  } 
+
+  const removeValue = ()=>{
+    if(counter < 0){ // counter vlaue not less than 0
+      counter = 0;
+    }else{
+      setCounter(counter--);
+    } 
+
+  }
+ 
+
+  return (
+    <> 
+
+     <h1> chai or react </h1> 
+     <h2>counter value : {counter} </h2>
+     <button 
+     onClick={addValue}>Add vlaue  {counter}</button>  
+     <br />
+     <button 
+     onClick={removeValue}>Remove value  {counter}</button> 
+
+     <p>footer : {counter}</p> 
+
+
+    </> 
+  )
+}
+
+export default App
