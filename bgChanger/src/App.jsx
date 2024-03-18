a1ad8a12430bb25ebf5105fbe57236372d26b9e9
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-03-19 02:15:09 +0530

    creating bgColor changer | Lecture - 9

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [color, setCOlor] = useState("olive")
+
+  return (
+    <>
+     <div className='w-full h-screen duration-200' 
+      style = {{backgroundColor: color}}>   
+
+      <div className='fixed flex flex-wrap justify-center  
+      bottom-12  inset-x-0 px-2'>  
+         <div className='flex flex-wrap justify-center gap-3  
+         shadow-lg bg-white px py-2 rounded-xl'> 
+
+         <button  
+         onClick={() => setCOlor("red")} 
+         className='outline-none px-4 rounded-full text-white shadow-lg'  
+         style = {{backgroundColor: "red"}}
+         >Red</button> 
+
+         <button  
+         onClick={() => setCOlor("green")} 
+         className='outline-none px-4 rounded-full text-white shadow-lg'  
+         style = {{backgroundColor: "green"}}
+         >Green</button>
+        
+          
+         <button   
+         onClick={() => setCOlor("black")}
+         className='outline-none  px-4 rounded-full text-white shadow-lg'  
+         style = {{backgroundColor: "black"}}
+         >Black</button>
+
+         <button   
+         onClick={() => setCOlor("blue")}
+         className='outline-none  px-4 rounded-full text-white shadow-lg'  
+         style = {{backgroundColor: "blue"}}
+         >Blue</button> 
+
+        <button   
+         onClick={() => setCOlor("purple")}
+         className='outline-none  px-4 rounded-full text-white shadow-lg'  
+         style = {{backgroundColor: "purple"}}
+         >Purple</button>
+         
+
+         </div>  
+
+          
+        
+
+
+      </div> 
+     </div>
+
+    </>
+  )
+}
+
+export default App
