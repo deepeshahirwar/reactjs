627907e4412c4e04f783b60456d30b120eeec9ff
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-03-21 18:38:48 +0530

    just completed player turn o , x

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+
+import './App.css'
+  
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares , setSquares] = useState(
+    Array(9).fill(null)); 
+
+    function handleClick(i){
+      if(squares[i]){// it means user click again filled box
+        return;
+      } 
+    
+      const nextSquares = squares.slice();// copy()
+      if(xIsNext){
+        nextSquares[i] = 'X';
+      }else{
+        nextSquares[i] = 'O';
+      } 
+
+      setSquares(nextSquares);// update current squares
+      setXIsNext(!xIsNext);// update current turn
+     
+    }
+
+  return (
+    <>  
+    
+<div className="game">
+    <div className='board-row'>  
+     <Square value={squares[0]} 
+     onSquareClick={()=> handleClick(0)}/>
+     <Square value={squares[1]} 
+     onSquareClick={()=> handleClick(1)}/>
+     <Square value={squares[2]} 
+     onSquareClick={()=> handleClick(2)}/>
+    </div> 
+
+    <div className='board-row'>  
+     <Square value={squares[3]} 
+     onSquareClick={()=> handleClick(3)}/>
+     <Square value={squares[4]} 
+     onSquareClick={()=> handleClick(4)}/>
+     <Square value={squares[5]} 
+     onSquareClick={()=> handleClick(5)}/>
+    </div>
+      
+    <div className='board-row'>  
+     <Square value={squares[6]} 
+     onSquareClick={()=> handleClick(6)}/>
+     <Square value={squares[7]} 
+     onSquareClick={()=> handleClick(7)}/>
+     <Square value={squares[8]} 
+     onSquareClick={()=> handleClick(8)}/>
+    </div>
+       
+    </div>
+      
+    </>
+  )
+}
+
+export default App
