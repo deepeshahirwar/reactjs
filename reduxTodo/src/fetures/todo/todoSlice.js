8e861c8ab6b3144b51ab88949ac3466b462b7176
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-04-14 17:42:32 +0530

    creating todo app with redux toolkit | lecture - 14

---
+++ reduxTodo/src/fetures/todo/todoSlice.js
@@ -0,0 +1,17 @@
+import { createSlice ,nanoid} from "@reduxjs/toolkit";
+ 
+const initialState = {
+    todos:[
+        {id: 1, text: "hello world"}
+    ]
+} 
+ 
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,  
+    reducers: {
+        addTodo : ()=> {},
+        removeTodo : ()=> {},
+        
+    }
+})
